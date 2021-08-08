713220c3ec579a3fbd98b687bd3d906e9cce8918
Rodrigo <rnavarro@accionpoint.com>
2021-08-08 17:55:00 -0300

    changes

--- dist/config/config.js
+++ dist/config/config.js
@@ -4,6 +4,7 @@ const dotenv_1 = require("dotenv");
 dotenv_1.config();
 exports.default = {
     //importamos el ENV y las exportamos para usar
+    port: process.env.PORT,
     secret: process.env.JWT_SECRET_KEY,
     tokenExpires: process.env.TOKEN_KEEP_ALIVE,
     mongoUser: process.env.MONGO_USER,
